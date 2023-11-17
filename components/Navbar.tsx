cd56440a46d93f5e0111c9015da53fd20f2b6794
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-11-17 17:51:09 +0100

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+// Image for faster loadtimes
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        {/* Image always need widtg and height properties */}
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            key={link.key}
+            href={link.href}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
